32e995410e81ecfd19eae74948cac6f93249cc86
SathvikIC9 <sathvikac810710833@gmail.com>
2025-06-21 16:48:40 +0530

    Add files via upload

---
+++ Day_01/Question_05.py
@@ -0,0 +1,11 @@
+# Write a function add_pair(d, key, value) that adds a key-value pair to dictionary d.
+
+intend= {
+    1:"harry",2:"IC"
+}
+def add_pair(intend, key, value):
+    intend[key]=value
+    print(intend)
+
+add_pair(intend, 3, "shub")
+    
