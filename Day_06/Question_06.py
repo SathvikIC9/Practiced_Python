d065dfa3d0babe3271991ddaab81c07a11a96bfb
SathvikIC9 <sathvikac810710833@gmail.com>
2025-06-29 19:56:47 +0530

    Add files via upload

---
+++ Day_06/Question_06.py
@@ -0,0 +1,6 @@
+a = ["apple", "banana", "cherry"]
+def joining():
+    joined = ",".join(a)
+    return joined
+print(f'"{joining()}"')
+# I know i have used a simple technique Tell me more suitable method to solve this problem.
