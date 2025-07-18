6673b8bcdae06b25582cbc00ba1846748c72545e
SathvikIC9 <sathvikac810710833@gmail.com>
2025-07-18 20:52:05 +0530

    Add files via upload

---
+++ LLMs/Day_07/dict_complete.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+app = Flask(__name__)
+@app.route('/complete')
+def spliting():
+        try:
+            value = "Hello world"
+            value = value.lower()
+            my_dict = {}
+            for word in value:
+                if word in my_dict:
+                    my_dict[word] += 1
+                else:
+                     my_dict[word] = 1
+            return f"{my_dict}"
+        except Exception as e :
+            return f'Error as {e}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
