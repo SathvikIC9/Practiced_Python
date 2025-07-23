7ce235a54b97278af7590610f30e36fd57a68796
SathvikIC9 <sathvikac810710833@gmail.com>
2025-07-23 20:31:36 +0530

    Add files via upload

---
+++ LLMs/Day_11/remember.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/remember', methods=['GET', 'POST'])
+def remember():
+    result = ""
+    if request.method == 'POST':
+        try:
+            value = request.form.get('value')  # matches input name
+            if value:
+                with open('remember.txt', 'w') as f:
+                    f.write(value.strip())
+                
+                with open("remember.txt", "r") as f1:
+                    name = f1.read()
+                
+                result = f"Hello there {name} 😁!"
+            else:
+                result = "PLEASE ENTER YOUR NAME 🙂💢"
+        except Exception:
+            result = "Oops"
+        
+    return f'''
+    <form method="post">
+        Enter your name: <input name="value" type="text">
+        <input type="submit">
+        <div>{result}</div>            
+    </form>
+    '''
+
+if __name__ == "__main__":
+    app.run(debug=True)
