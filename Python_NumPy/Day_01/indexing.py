862bf05a62e34553a0d2f15998f68735912d1d70
SathvikIC9 <sathvikac810710833@gmail.com>
2025-08-13 20:50:51 +0530

    Add files via upload

---
+++ Python_NumPy/Day_01/indexing.py
@@ -0,0 +1,11 @@
+import numpy as np
+x = np.array([10,20,30,40,50])
+print(x[2]) # single indexing 
+
+print(x[-1])# negative indexing 
+
+# multi dimentional array access
+
+y = np.array([[1,2],[3,4]])
+print(y[1,0])# finding an element in multiple arrays ... where 1 is the index of the set and 0 is the element index in the set...
+
